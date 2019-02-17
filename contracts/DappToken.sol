5966c89e687c516026a78d156c71bebc90db4112
imangas <ignaciomangas@gmail.com>
2019-02-17 17:05:26 +0100

    added transfer method and updated tests

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.4.21 <0.5.0;
 
 contract DappToken {
     string public name = "DappToken";
@@ -6,10 +6,27 @@ contract DappToken {
     string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _value) ;
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -29,4 +29,34 @@ contract('DappToken', function(accounts) {
             assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account')
         })
     })
+
+    it('fails if balance is not enough', () => {
+        return DappToken.deployed().then( (instance) => {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 99999999999999999999999999999);
+        }).then(assert.fail).catch((error) => {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+        })
+    })
+
+    it('transfers token ownership', () => {
+        return DappToken.deployed().then( (instance) => {
+            tokenInstance = instance;
+            return tokenInstance.transfer(accounts[1], 25000, {
+                from: accounts[0]
+            });
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'triggers one event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'triggers one event');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'triggers one event');
+            assert.equal(receipt.logs[0].args._value, 25000, 'triggers one event');
+            return tokenInstance.balanceOf(accounts[1])
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), 25000, 'adds the amount to the receiving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then((balance)=>{
+            assert.equal(balance.toNumber(), (1000000-25000), 'deduce the amount from the sender account');
+        })
+    })
 })
