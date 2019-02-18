7c2f69d2d0e694225aa7030a007bd6eb23d94f6c
iMangas <ignacio@herotoken.io>
2019-02-18 21:54:28 +0100

    wip

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,7 +12,14 @@ contract DappToken {
         uint256 _value
     );
 
+    event Approval (
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256) ) public allowance;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -28,5 +35,15 @@ contract DappToken {
         emit Transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _Value) public returns (bool success) {
+        
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -59,4 +59,37 @@ contract('DappToken', function(accounts) {
             assert.equal(balance.toNumber(), (1000000-25000), 'deduce the amount from the sender account');
         })
     })
+
+    it('approves tokens for deletaged transfer', ()=> {
+        return DappToken.deployed().then( (instance) => {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then((success) => {
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.approve(accounts[1], 100);
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1] );
+        }).then((allowance) => {
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegate transfer');
+        })
+    })
+
+    it('handles delegated token transfers ', () => {
+        return DappToken.deployed().then( (instance) => {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+        }).then((receipt) => {
+            return tokenInstance.approve(spendingAccount, 10, { from: fromAccount});
+        }).then((receipt) => {
+            return tokenInstance.trasferFrom(fromAccount, toAccount, 99999, {from: spendingAccount});
+        })
+    })
 })
